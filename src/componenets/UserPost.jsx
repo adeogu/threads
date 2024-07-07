0f007def59c39a9baa15aab130b166c8c397d702
adeogu <adeogu1234@gmail.com>
2024-07-07 20:45:12 +0100

    kk

--- src/componenets/UserPost.jsx
+++ src/componenets/UserPost.jsx
@@ -4,8 +4,11 @@ import { Image } from "@chakra-ui/image";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { BsThreeDots } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Actions from "./Actions";
+import { useState } from "react";
 
 const UserPost = () => {
+	const [liked, setLiked] = useState(false);
 	return (
 		<Link to={"/markzuckerberg/post/1"}>
 			<Flex gap={3} mb={4} py={5}>
@@ -66,6 +69,9 @@ const UserPost = () => {
 					>
 						<Image src="/post1.png" w={"full"} />
 					</Box>
+					<Flex gap={3} my={1}>
+						<Actions liked={liked} setLiked={setLiked} />
+					</Flex>
 				</Flex>
 			</Flex>
 		</Link>
