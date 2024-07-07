7363f9d1f52d6cfa9afc9a69a174dc10243213a8
adeogu <adeogu1234@gmail.com>
2024-07-08 00:06:26 +0100

    ff

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,65 @@
-import { Avatar, Flex, Image, Text, Box } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
+// import { Avatar, Flex, Image, Text, Box, Divider } from "@chakra-ui/react";
+// import { BsThreeDots } from "react-icons/bs";
+// import { useState } from "react";
+// import Actions from "./componenets/Actions";
+
+// const PostPage = () => {
+// 	const [liked, setLiked] = useState(false);
+
+// 	return (
+// 		<>
+// 			<Flex>
+// 				<Flex w={"full"} alignItems={"center"} gap={3}>
+// 					<Avatar src="/zuck-avatar.png" size={"md"} name="Mark Zuckerberg" />
+// 					<Flex>
+// 						<Text fontSize={"sm"} fontWeight={"bold"}>
+// 							markzuckerberg
+// 						</Text>
+// 						<Image src="/verified.png" w="4" h="4" ml={4} />
+// 					</Flex>
+// 				</Flex>
+// 				<Flex gap={4} alignItems={"center"}>
+// 					<Text fontSize={"sm"} color={"gray.500"}>
+// 						1d
+// 					</Text>
+// 					<BsThreeDots />
+// 				</Flex>
+// 			</Flex>
+
+// 			<Text my={3}>Let's talk about threads</Text>
+// 			<Box
+// 				borderRadius={6}
+// 				overflow={"hidden"}
+// 				border={"1px solid"}
+// 				borderColor={"gray.200"}
+// 			>
+// 				<Image src={"/post1.png"} w={"full"} />
+// 			</Box>
+
+// 			<Flex gap={3} my={3}>
+// 				<Actions liked={liked} setLiked={setLiked} />
+// 			</Flex>
+
+// 			<Flex gap={2} alignItems={"center"}>
+// 				<Text color={"gray.500"} fontSize={"sm"}>
+// 					238 Replies
+// 				</Text>
+
+// 				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.500"}></Box>
+
+// 				<Text color={"gray.500"} fontSize={"sm"}>
+// 					{200 + (liked ? 1 : 0)} likes
+// 				</Text>
+// 			</Flex>
+// 			<Divider my={4} />
+// 		</>
+// 	);
+// };
+
+// export default PostPage;
+
 import { useState } from "react";
-import Actions from "./Actions";
+import Actions from "./components/Actions";
 
 const PostPage = () => {
 	const [liked, setLiked] = useState(false);
@@ -19,19 +77,19 @@ const PostPage = () => {
 					</Flex>
 				</Flex>
 				<Flex gap={4} alignItems={"center"}>
-					<Text fontSize={"sm"} color={"gray.light"}>
+					<Text fontSize={"sm"} color={"gray.500"}>
 						1d
 					</Text>
 					<BsThreeDots />
 				</Flex>
 			</Flex>
 
-			<Text my={3}>Lets *apos; talk anout threads</Text>
+			<Text my={3}>Let's talk about threads</Text>
 			<Box
 				borderRadius={6}
 				overflow={"hidden"}
 				border={"1px solid"}
-				borderColor={"gray.light"}
+				borderColor={"gray.200"}
 			>
 				<Image src={"/post1.png"} w={"full"} />
 			</Box>
@@ -41,13 +99,13 @@ const PostPage = () => {
 			</Flex>
 
 			<Flex gap={2} alignItems={"center"}>
-				<Text color={"gray.light"} fontSize={"sm"}>
+				<Text color={"gray.500"} fontSize={"sm"}>
 					238 Replies
 				</Text>
 
-				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"grey.light"}></Box>
+				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.500"}></Box>
 
-				<Text color={"gray.light"} fontSize={"sm"}>
+				<Text color={"gray.500"} fontSize={"sm"}>
 					{200 + (liked ? 1 : 0)} likes
 				</Text>
 			</Flex>
